be9c5c4eb9f5de4b9e80954c52011d30581fd120
Ezekiel E <ezekielediale@gmail.com>
2020-09-15 21:38:48 +0100

    initial setup

---
+++ js/cos.js
@@ -0,0 +1,13 @@
+function closeHere() {
+  m = document.getElementById("oPara");
+ m.className="closed";
+//m.style.display="none";
+}
+
+
+
+function openHere() {
+  m = document.getElementById("oPara");
+  m.className="open";
+
+}
